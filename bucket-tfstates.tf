f9f92618b69496321b6a48e10847ff44e62722a3
aminvielledebatAtBedrock <107035817+aminvielledebatAtBedrock@users.noreply.github.com>
2023-05-03 12:06:52 +0200

    feat: add deleteObject action to tech role (#5)

--- bucket-tfstates.tf
+++ bucket-tfstates.tf
@@ -68,6 +68,7 @@ data "aws_iam_policy_document" "bucket_tfstates_policy" {
       "s3:ListBucket",
       "s3:PutObject",
       "s3:GetObject",
+      "s3:DeleteObject",
     ]
 
     resources = [
